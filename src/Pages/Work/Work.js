df20e1783469f239f37b8eec1a472ac93129cf59
MaxSantana92 <maximiliano@lowa.com.ar>
2021-04-22 15:31:16 -0300

    marco de muestra de comerciales

--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -1,6 +1,5 @@
-import React/* , { useState }  */from 'react';
+import React, { useEffect, useState } from 'react';
 import Audible from '../../Static/Circulo-Links/AUDIBLE.svg';
-//import AudibleVideo from '../../Static/Video/AudibleEEUUSpanish.mp4'
 import Avene from '../../Static/Circulo-Links/AVENE.svg';
 import CocaCola from '../../Static/Circulo-Links/COCACOLA.svg';
 import Disney from '../../Static/Circulo-Links/DISNEY.svg';
@@ -11,34 +10,41 @@ import Sprite from '../../Static/Circulo-Links/SPRITE.svg';
 import Honda from '../../Static/Circulo-Links/HONDA.svg';
 import Pantene from '../../Static/Circulo-Links/PANTENE.svg';
 import manchaWork from '../../Static/Mancha-home1.gif';
+import CocaColaVideo from '../../Static/Video/CocaCola.mp4';
 import './Work.css';
 
 const Work = () => {
     
-    //const [videoSeleccionado, setVideoSeleccionado] = useState("");
     
     
-    function mostrarVideo(id){
-        /* switch(id){
-            case "Audible":{setVideoSeleccionado(AudibleVideo);}
-        } */
-         
-
+    function mostrarVideo(){
+        
+        const marcoVideoComercial =document.querySelector('.marcoVideo');
+        
         document.querySelector('.contenedorVideosWork').classList.add("aperturaComerciales");;
         document.querySelector('.contenidoVideo').volume = 0.1;
-        //document.querySelector('.contenidoVideo').classList.add("animate__animated", "animate__fadeIn", "animate__slow", "animate__delay-2s");
-        setTimeout(()=>{
-            document.querySelector('.marcoVideo').style.width ="70%";
-            document.querySelector('.marcoVideo').style.height ="70%";
-            document.querySelector('.marcoVideo').style.display ="initial";
-            document.querySelector('.marcoVideo').style.borderradius ="0";
-            document.querySelector('.marcoVideo').classList.add("marcoRectangular");
-        },2000);
+        
+        marcoVideoComercial.style.width ="70%";
+        marcoVideoComercial.style.height ="70%";
+        marcoVideoComercial.style.display ="initial";
+        marcoVideoComercial.style.borderradius ="0";
+        marcoVideoComercial.classList.add("marcoRectangular");
     };
 
     const cerrarComerciales = () =>{
         document.querySelector('.contenedorVideosWork').classList.remove("aperturaComerciales");
+        document.querySelector('.contenidoVideo').pause();
     };
+
+    
+    /* const retornaVideoComercial = (url) => {
+
+        return (
+            <video className="contenidoVideo" controls>
+                <source type="video/mp4" src={url}></source>
+            </video>
+        )
+    } */
 
     return (
         <div className="work">
@@ -46,9 +52,11 @@ const Work = () => {
 
                 <div className="marcoVideo">
 
-                    {/* <video className="contenidoVideo" controls  muted>
-                        <source type="video/mp4" src={AudibleVideo}></source>
-                    </video> */}
+                    <video className="contenidoVideo" controls>
+                        <source type="video/mp4" src={CocaColaVideo}></source>
+                    </video>
+                    
+
     
                 </div>
                 <button onClick={()=>cerrarComerciales()} className="contenedorCerrarComercialesWork">
@@ -72,16 +80,16 @@ const Work = () => {
                 </div>
 
                 <div className="contenedor-Links rotar">
-                    <div><img id="Audible" onClick={(e)=>mostrarVideo(e.target.id)} className="audible cursorLink" alt="#" src={Audible}></img></div>
-                    <div><img onClick={(e)=>mostrarVideo(e)} className="avene cursorLink" alt="#" src={Avene}></img></div>
-                    <div><img id="CocaCola" onClick={(e)=>mostrarVideo(e)} className="coca cursorLink" alt="#" src={CocaCola}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="disney cursorLink" alt="#" src={Disney}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="fanta cursorLink" alt="#" src={Fanta}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="ladysoft cursorLink" alt="#" src={Ladysoft}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="lays cursorLink" alt="#" src={Lays}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="sprite cursorLink" alt="#" src={Sprite}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="honda cursorLink" alt="#" src={Honda}></img></div>
-                    <div><img onClick={()=>mostrarVideo()} className="pantene cursorLink" alt="#" src={Pantene}></img></div>
+                    <div><img id="audible" onClick={(e)=>mostrarVideo(e.target.id)} className="audible cursorLink" alt="#" src={Audible}></img></div>
+                    <div><img id="avene" onClick={(e)=>mostrarVideo(e.target.id)} className="avene cursorLink" alt="#" src={Avene}></img></div>
+                    <div><img id="cocacola" onClick={(e)=>mostrarVideo(e.target.id)} className="coca cursorLink" alt="#" src={CocaCola}></img></div>
+                    <div><img id="disney" onClick={(e)=>mostrarVideo(e.target.id)} className="disney cursorLink" alt="#" src={Disney}></img></div>
+                    <div><img id="fanta" onClick={(e)=>mostrarVideo(e.target.id)} className="fanta cursorLink" alt="#" src={Fanta}></img></div>
+                    <div><img id="ladysoft" onClick={(e)=>mostrarVideo(e.target.id)} className="ladysoft cursorLink" alt="#" src={Ladysoft}></img></div>
+                    <div><img id="lays" onClick={(e)=>mostrarVideo(e.target.id)} className="lays cursorLink" alt="#" src={Lays}></img></div>
+                    <div><img id="sprite" onClick={(e)=>mostrarVideo(e.target.id)} className="sprite cursorLink" alt="#" src={Sprite}></img></div>
+                    <div><img id="honda" onClick={()=>mostrarVideo()} className="honda cursorLink" alt="#" src={Honda}></img></div>
+                    <div><img id="pantene" onClick={(e)=>mostrarVideo(e.target.id)} className="pantene cursorLink" alt="#" src={Pantene}></img></div>
                 </div>
             </div>
         </div>
