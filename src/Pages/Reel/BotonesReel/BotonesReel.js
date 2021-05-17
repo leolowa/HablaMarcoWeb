89260c05eb1331751f9a002f35e8d639f864b8fc
MaxSantana92 <maximiliano@lowa.com.ar>
2021-05-17 08:54:25 -0300

    implementación para descarga de Reel

--- src/Pages/Reel/BotonesReel/BotonesReel.js
+++ src/Pages/Reel/BotonesReel/BotonesReel.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import iconoPause from '../../../Static/iconoPause.svg';
+import reelLatam from '../../../Static/Sound/habla-marco-reel-latam-2.mp3';
+import reelArg from '../../../Static/Sound/habla-marco-reelArg.mp3';
+
+
 
 export const BotonesReel = ({onPlay, TextoReel="Reel", isPaused=false, tipoReel=''}) => {
-    
+    const   descargarArchivoReel =  (tipo) =>{
+        if(tipo !== 'ReelLatam'){
+            return reelLatam;}
+        else{ return reelArg}
+    }
     return (
         <div className="contenedorSubReel">
             <button onClick={onPlay} className="botonReelPlay">
@@ -17,7 +25,7 @@ export const BotonesReel = ({onPlay, TextoReel="Reel", isPaused=false, tipoReel=
 
             <div className="reelDescarga">
                 <h6 className="Texto-Reel">{TextoReel}</h6>
-                <a href={tipoReel} download="Reel"  className="botonReelPlay">
+                <a href={tipoReel} download={descargarArchivoReel(tipoReel)}  className="botonReelPlay">
                     <i className="bi bi-arrow-down iconoDescarga"></i>
                 </a>
             </div>
