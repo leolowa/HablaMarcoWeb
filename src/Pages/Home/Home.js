a056d015c45a22c1e8857c99645714ff77c8882f
MaxiLowa <maximiliano@lowa.com.ar>
2021-11-11 14:56:43 -0300

    feat: SECCION INICIO: Re componer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,35 +1,31 @@
 import React from 'react';
 import HablaMarco from '../../Static/HablaMarco.svg';
-//import marcoNadando from '../../Static/Video/marcoNadando.mov';
+import baileConPerro from '../../Static/Video/baileConPerro.mp4';
 import './Home.css';
 
 const Home = () => {
-
   return (
     <React.Fragment>
       <div className="Home">
-    
         <div id="home" className="body-Home animate__animated animate__fadeIn">
-        
           <div className="contenedor-Marca">
             <img alt="" className="Marca" src={HablaMarco}></img>
           </div>
-          
+
           <div className="contenedor-Scroll">
             <i className="bi bi-caret-down-fill icono-Scroll"></i>
           </div>
-            
-          <div className="contenedorMarcoNadando">
-            {/* <video className="videoMarcoNadando" autoPlay={true} muted="muted" loop>
-              <source type="video/mp4" src={marcoNadando}></source>
-            </video> */}
-          </div>
-        </div>
 
+          <div className="contenedorVideoInicio">
+            <video className="videoMarcoNadando" autoPlay={true} muted="muted" loop>
+              <source type="video/mp4" src={baileConPerro}></source>
+            </video>
+          </div>
+          <div className="circuloRojo-Inicio"></div>
+        </div>
       </div>
- 
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Home;
