f45388a0390c2f29867d23b1d0eafd980afb4c1f
MaxiLowa <maximiliano@lowa.com.ar>
2021-11-11 08:45:37 -0300

    feat: Cambiar texto WORK por Voces y HOME por inicio

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -2,58 +2,53 @@ import React from 'react';
 import './Menu.css';
 import flechaMenu from '../../Static/flechaMenu.svg';
 
-
 const Menu = ({tipoIconoMenu}) => {
-
-    function redireccionar(id){
-        const menu = document.querySelector('.Menu');
-        menu.style.width="0vw";
-        const redireccionar = document.querySelector(`#${id}`);
-        redireccionar.scrollIntoView();
-        if((redireccionar.id==='about') || (redireccionar.id==='contacto')){
-            tipoIconoMenu('blanco');
-        }
-        else{
-            tipoIconoMenu('negro')
-        }
-   }
-
-   const cerrarMenu = ()=>{
+  function redireccionar(id) {
     const menu = document.querySelector('.Menu');
-    menu.style.width="0vw"; 
-   };
+    menu.style.width = '0vw';
+    const redireccionar = document.querySelector(`#${id}`);
+    redireccionar.scrollIntoView();
+    if (redireccionar.id === 'about' || redireccionar.id === 'contacto') {
+      tipoIconoMenu('blanco');
+    } else {
+      tipoIconoMenu('negro');
+    }
+  }
 
-    return (
-                <div className="Menu">
-                    <div onClick={()=>cerrarMenu()} className="iconoCerrar">
-                         <img alt="" src={flechaMenu}></img>
-                    </div>
+  const cerrarMenu = () => {
+    const menu = document.querySelector('.Menu');
+    menu.style.width = '0vw';
+  };
 
-                    <div className="bodyMenu">
+  return (
+    <div className="Menu">
+      <div onClick={() => cerrarMenu()} className="iconoCerrar">
+        <img alt="" src={flechaMenu}></img>
+      </div>
 
-                        <div onClick={()=>redireccionar("home")} >
-                            <h1 className="TextoMenu subrayado">HOME</h1>
-                        </div>
+      <div className="bodyMenu">
+        <div onClick={() => redireccionar('home')}>
+          <h1 className="TextoMenu subrayado">INICIO</h1>
+        </div>
 
-                        <div onClick={()=>redireccionar("work")}> 
-                            <h1 className="TextoMenu subrayado">WORK</h1>
-                        </div>
+        <div onClick={() => redireccionar('work')}>
+          <h1 className="TextoMenu subrayado">VOCES</h1>
+        </div>
 
-                        <div onClick={()=>redireccionar("reel")} > 
-                            <h1 className="TextoMenu subrayado">REEL</h1>
-                        </div>
+        <div onClick={() => redireccionar('reel')}>
+          <h1 className="TextoMenu subrayado">REEL</h1>
+        </div>
 
-                        <div onClick={()=>redireccionar("about")} > 
-                            <h1 className="TextoMenu subrayado">MARCO</h1>
-                        </div>
+        <div onClick={() => redireccionar('about')}>
+          <h1 className="TextoMenu subrayado">MARCO</h1>
+        </div>
 
-                        <div onClick={()=>redireccionar("contacto")} > 
-                            <h1 className="TextoMenu subrayado">CONTACTO</h1>
-                        </div>
-                        
-                    </div>
-                </div>
-    )
-}
+        <div onClick={() => redireccionar('contacto')}>
+          <h1 className="TextoMenu subrayado">CONTACTO</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
