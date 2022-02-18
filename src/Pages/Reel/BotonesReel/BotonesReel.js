2bbcdf18f5d03d30d6e132478b7f12d66f708a61
MaxSantana <maxsantana92ok@gmail.com>
2022-02-18 17:30:27 -0300

    Corrección de tipo de reel

--- src/Pages/Reel/BotonesReel/BotonesReel.js
+++ src/Pages/Reel/BotonesReel/BotonesReel.js
@@ -3,32 +3,36 @@ import iconoPause from '../../../Static/iconoPause.svg';
 import reelLatam from '../../../Static/Sound/habla-marco-reel-latam-2.mp3';
 import reelArg from '../../../Static/Sound/habla-marco-reelArg.mp3';
 
-
-
-export const BotonesReel = ({onPlay, isDisabled, TextoReel="Reel", isPaused=false, tipoReel=''}) => {
-    const   descargarArchivoReel =  (tipo) =>{
-        if(tipo !== 'ReelLatam'){
-            return reelLatam;}
-        else{ return reelArg}
+export const BotonesReel = ({
+  onPlay,
+  isDisabled,
+  TextoReel = 'Reel',
+  isPaused = false,
+  tipoReel = '',
+}) => {
+  const descargarArchivoReel = tipo => {
+    if (tipo === 'ReelLatam') {
+      return reelLatam;
+    } else {
+      return reelArg;
     }
-    return (
-        <div className="contenedorSubReel">
-            <button disabled={isDisabled} onClick={onPlay} className="botonReelPlay">
-                {
-                    isPaused
-                    ?
-                    <img alt='' src={iconoPause} className="iconoReel"></img>
-                    :
-                    <i className="bi bi-play iconoReel"></i>
-                }
-            </button>
+  };
+  return (
+    <div className="contenedorSubReel">
+      <button disabled={isDisabled} onClick={onPlay} className="botonReelPlay">
+        {isPaused ? (
+          <img alt="" src={iconoPause} className="iconoReel"></img>
+        ) : (
+          <i className="bi bi-play iconoReel"></i>
+        )}
+      </button>
 
-            <div className="reelDescarga">
-                <h6 className="Texto-Reel">{TextoReel}</h6>
-                <a href={tipoReel} download={descargarArchivoReel(tipoReel)}  className="botonReelPlay">
-                    <i className="bi bi-arrow-down iconoDescarga"></i>
-                </a>
-            </div>
-        </div>
-    )
-}
+      <div className="reelDescarga">
+        <h6 className="Texto-Reel">{TextoReel}</h6>
+        <a href={tipoReel} download={descargarArchivoReel(tipoReel)} className="botonReelPlay">
+          <i className="bi bi-arrow-down iconoDescarga"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
